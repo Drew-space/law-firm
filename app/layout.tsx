9c8d3605592dd3ec144048cdfb7ec635e741552a
Drew <chukwukaenudeme@gmail.com>
2026-04-29 11:47:49 +0100

    refactor: clean up layout files by removing unused imports and commented code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,3 @@
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono, Inter, Mona_Sans } from "next/font/google";
-
-// import { cn } from "@/lib/utils";
-
-// export const metadata: Metadata = {
-//   title: "law firm",
-//   description: "find a lawyer online.",
-// };
-
-// export default function MainLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <div className={cn("h-full antialiased font-sans")}>
-//       <main className="">{children}</main>
-//     </div>
-//   );
-// }
-
 import { Bebas_Neue, Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "@/app/globals.css";
