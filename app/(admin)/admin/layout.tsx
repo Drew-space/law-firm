78830d2cf4266095ee502e27c0bf5f6a992db3d1
Drew <chukwukaenudeme@gmail.com>
2026-05-02 06:04:01 +0100

    feat(admin): add new pages for cases, scheduled, and users with dynamic titles

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar";
+"use client";
 import {
   SidebarInset,
   SidebarProvider,
@@ -8,12 +8,20 @@ import { Separator } from "@/components/ui/separator";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { UserButton } from "@clerk/nextjs";
 import { AdminSidebar } from "@/components/admin-sidebar";
-
+import { usePathname } from "next/navigation";
+const pageTitles: Record<string, string> = {
+  "/admin": "Dashboard",
+  "/admin/cases": "All Cases",
+  "/admin/users": "Users",
+  "/admin/scheduled": "Scheduled",
+};
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const title = pageTitles[pathname] ?? "Admin";
   return (
     <TooltipProvider>
       <SidebarProvider>
@@ -26,7 +34,7 @@ export default function AdminLayout({
               className="mr-2 data-vertical:h-4 data-vertical:self-auto"
             />
             <div className="flex justify-between w-full items-center">
-              <h1 className="text-sm font-medium">Admin</h1>
+              <h1 className="text-sm font-medium">{title} </h1>
               <UserButton />
             </div>
           </header>
