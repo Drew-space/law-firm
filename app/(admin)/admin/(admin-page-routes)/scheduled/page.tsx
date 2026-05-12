77584adb24d856f64a3081e17c62083409b7e303
Drew <chukwukaenudeme@gmail.com>
2026-05-12 22:00:06 +0100

    fix: update greeting text in scheduled page component

--- app/(admin)/admin/(admin-page-routes)/scheduled/page.tsx
+++ app/(admin)/admin/(admin-page-routes)/scheduled/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>hello from page jhghghj</div>;
+  return <div>hello from page j manfdjhjkghjgef hghghj</div>;
 };
 
 export default page;
